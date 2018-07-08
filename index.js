1b8bc99074d6227ef7d6cf8f96069fae74f6a049
Charles Kim <cwkim3@gmail.com>
2018-07-08 00:04:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ var recipes = {key:"value"};
 function updateObjectWithKeyAndValue(recipes, key, value){
   return Object.assign({}, recipes, { [key]: value} );
 }
+function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value){
+  recipes[key] = 
+}
